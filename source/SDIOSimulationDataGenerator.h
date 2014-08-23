50d1588077b6d41fec31a611f36dc94f452dd636
Jonathan Conway <jonco29@gmail.com>
2014-08-23 09:13:29 -0500

    working on simulator, making progress, not there yet

--- source/SDIOSimulationDataGenerator.h
+++ source/SDIOSimulationDataGenerator.h
@@ -2,6 +2,7 @@
 #define SDIO_SIMULATION_DATA_GENERATOR
 
 #include <SimulationChannelDescriptor.h>
+#include <AnalyzerHelpers.h>
 #include <string>
 class SDIOAnalyzerSettings;
 
@@ -22,8 +23,35 @@ protected:
 	void CreateSerialByte();
 	std::string mSerialText;
 	U32 mStringIndex;
+    ClockGenerator generator;
 
 	SimulationChannelDescriptor mSerialSimulationData;
+
+    SimulationChannelDescriptorGroup mSdioSimulationChannels;
+	SimulationChannelDescriptor *mSimClk;
+	SimulationChannelDescriptor *mSimCmd;
+	SimulationChannelDescriptor *mSimData0;
+	SimulationChannelDescriptor *mSimData1;
+	SimulationChannelDescriptor *mSimData2;
+	SimulationChannelDescriptor *mSimData3;
+
+    U32 currentDataIndex;
+    U32 numBitsRemaining;
+    bool isDataLine;
+
+public:
+    class DataRepresentation
+    {
+        private: 
+            U32 currentDataIndex;
+            U32 numBitsRemaining;
+            bool isDataLine;
+            void setBits();
+        public:
+            DataRepresentation ();
+    };
+
+    static const U64 sampleData[598];
 
 };
 #endif //SDIO_SIMULATION_DATA_GENERATOR
